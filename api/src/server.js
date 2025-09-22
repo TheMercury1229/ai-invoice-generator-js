ee4e203f0a19bb5b9286846c874e32f6e80950e6
Hardik Gujrathi <gujarathihardik29@gmail.com>
2025-09-23 00:16:42 +0530

    feat: Set up MongoDB connection and define user and invoice models

--- api/src/server.js
+++ api/src/server.js
@@ -1,1 +1,31 @@
-console.log("Hello world");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.js";
+dotenv.config();
+const app = express();
+
+//Middelwares to handle cors
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Connect Database
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes
+
+// Start Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
